7bcfedbb12e6f4cbfbec338876f84b04ba78db9c
lqs1848 <253612442@qq.com>
2020-03-18 16:48:56 +0800

    People

---
+++ StartDownload/AvCoverDownloader/Collector.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvCoverDownloader
+{
+    class Collector
+    {
+        String basePath = "https://www.dmmsee.cloud";
+        public EventHandler<String> CollectorLog;
+        public EventHandler<String> ErrorLog;
+        SynchronizationContext _syncContext;
+        //HttpClient client;
+
+        //线程数
+        const int cycleNum = 10;
+
+        string filePath = null;
+        
+        public Collector(SynchronizationContext formContext,string filePath)
+        {
+            _syncContext = formContext;
+            this.filePath = filePath;
+        }
+
+        public void Start()
+        {
+            new Thread(Collect).Start();
+        }
+
+        Queue<int> q = new Queue<int>();
+
+        public void Collect()
+        {
+            for (int i = 1; i <= 250; i++)
+            {
+                q.Enqueue(i);
+            }
+            ThreadPool.SetMinThreads(cycleNum, cycleNum);
+            ThreadPool.SetMaxThreads(cycleNum, cycleNum);
+            for (int i = 1; i <= cycleNum; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(DownloadFun));
+            }
+        }//method 
+
+        public void DownloadFun(object o)
+        {
+            while (q.Count > 0) {
+                int i = q.Dequeue();
+                string url = basePath + "/actresses/" + i;
+                string html = Download.GetHtml(url);
+                MatchCollection mcs = new Regex(@"<img\ssrc=""(.*?)""\stitle=""(.*?)"">").Matches(html);
+                foreach (Match m in mcs) {
+
+                    string startName = GetPath(m.Groups[2].Value.Trim());
+                    string startPhoto = m.Groups[1].Value.Trim();
+                    if (startPhoto.IndexOf("nowprinting") == -1) {
+                        string pr = startName.Substring(0, 1);
+                        string dirstr = filePath + "\\" + pr + "\\"+ startName+"\\";
+                        Download.HttpDownloadFile(startPhoto, dirstr, "poster.jpg");
+                        _syncContext.Post(OutLog, startName);
+                    }
+                }//foreach
+            }
+            _syncContext.Post(OutLog, "线程退出");
+        }//method
+
+
+        private void OutLog(object state)
+        {
+            CollectorLog?.Invoke(this, state.ToString());
+        }
+
+        private void OutError(object state)
+        {
+            ErrorLog?.Invoke(this, state.ToString());
+        }
+
+        private string GetPath(string newfolderName) {
+            newfolderName = newfolderName.Replace("\\", "‖");
+            newfolderName = newfolderName.Replace("/", "‖");
+            newfolderName = newfolderName.Replace(":", "：");
+            newfolderName = newfolderName.Replace("*", "※");
+            newfolderName = newfolderName.Replace("?", "？");
+            newfolderName = newfolderName.Replace("<", "〈");
+            newfolderName = newfolderName.Replace(">", "〉");
+            newfolderName = newfolderName.Replace("\n", " ");
+            newfolderName = newfolderName.Replace("\r", " ");
+            //newfolderName = Regex.Replace(newfolderName, @"[/n/r]", " ");
+
+            StringBuilder rBuilder = new StringBuilder(newfolderName);
+            foreach (char rInvalidChar in Path.GetInvalidPathChars())
+                rBuilder.Replace(rInvalidChar.ToString(), string.Empty);
+            return rBuilder.ToString();
+        }
+
+    }//class
+}
---
+++ StartDownload/AvCoverDownloader/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvCoverDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dilog.Description = "请选择路径";
+            DialogResult res = dilog.ShowDialog();
+            if (res == DialogResult.OK || res == DialogResult.Yes)
+            {
+                textBox1.Text = dilog.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox3.AppendText("\r\n---下载日志---\r\n");
+            button3.Enabled = false;
+            Collector cl = new Collector(SynchronizationContext.Current, textBox1.Text);
+            cl.CollectorLog += (o, text) =>
+            {
+                this.textBox4.AppendText(text + "\r\n");
+            };
+            cl.ErrorLog += (o, text) =>
+            {
+                this.textBox3.AppendText(text + "\r\n");
+            };
+            cl.Start();
+        }
+
+        int getNumberIndex(string fh)
+        {
+            char[] chars = fh.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (Char.IsNumber(chars[i]))
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
