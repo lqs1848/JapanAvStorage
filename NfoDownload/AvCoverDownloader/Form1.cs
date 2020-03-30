7a77a92a23c2ef70b96853eae3ec4ba118b7ca9c
lqs1848 <253612442@qq.com>
2020-03-30 11:38:28 +0800

    bug

--- NfoDownload/AvCoverDownloader/Form1.cs
+++ NfoDownload/AvCoverDownloader/Form1.cs
@@ -47,173 +47,20 @@ namespace AvCoverDownloader
             string[] folders = Directory.GetDirectories(textBox1.Text);
             foreach (string folder in folders)
             {
-               int fgg = folder.LastIndexOf("\\") + 1;
+                int fgg = folder.LastIndexOf("\\") + 1;
                 string name = folder.Substring(fgg, folder.Length - fgg).Trim();
-                //Match match = new Regex(@"([A-Za-z0-9]+[-_]*?[A-Za-z0-9]+)\s*").Match(name);
-                //Match match = new Regex(@"([A-Za-z0-9]+[-_]*[A-Za-z0-9]+[-_]*[A-Za-z0-9]*)\s*").Match(name);
-                /*Match dateMatch = new Regex("[0-9]{4}-[0-9]{2}-[0-9]{2}").Match(name);
-                if (dateMatch.Success) {
-                    name = name.Replace(dateMatch.Groups[0].Value, "");
-                } */
-                name = name.Split(' ')[1];
-                Av av = new Av();
-                av.Id = name;
-                av.Path = folder;
-                avs.Add(av);
-                textBox2.AppendText("->" + name + "\r\n");
-                /*
-                match = new Regex(@"(?:[0-9]{4}-[0-9]{2}-[0-9]{2})?\s?([A-Za-z0-9]+[-_]*[A-Za-z0-9]+[-_]*[A-Za-z0-9]*)\s*").Match(name);
-                if (match.Success) {
-                    string matRes = match.Groups[1].Value;
-                    textBox2.AppendText(matRes + "\r\n");
+                try {
+                    name = name.Split(' ')[1];
                     Av av = new Av();
-                    av.Id = matRes.Trim();
+                    av.Id = name;
                     av.Path = folder;
                     avs.Add(av);
-                }//match sucess
-                else
-                    textBox3.AppendText(name + "-无法解析番号\r\n");*/
-/*
-                textBox2.AppendText(name + "\r\n");
-                MatchCollection matchs = new Regex("[A-Za-z0-9]+[-_]?[A-Za-z0-9]+([-_]?[0-9])*").Matches(name);
-                foreach (Match match in matchs)
-                {
-                    string fh = match.Groups[0].Value.Trim();
-                    if (fh.Length < 5) continue;
-                    //Match mat = new Regex(@"^[A-Za-z0-9-_;]+$").Match(fh);
-                    //if (!mat.Success) continue;
-                    int numIndex = getNumberIndex(fh);
-                    if (numIndex == -1) continue;
-
-                    Av av = new Av();
-                    av.Id = fh;
-                    av.Path = folder;
-                    avs.Add(av);
-                    textBox2.AppendText("->" + fh + "\r\n");
-                    IdSet.Add(fh);
-
-                    if (fh.IndexOf("-1") != -1 || fh.IndexOf("-2") != -1)
-                    {
-                        int fg = fh.LastIndexOf("-");
-                        fh = fh.Substring(0, fg).Trim();
-                        if (fh.Length < 6) continue;
-
-                        if (IdSet.Contains(fh)) continue;
-                        av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-
-                    }
-                    else if (fh.IndexOf("-") == -1 && fh.IndexOf("_") == -1)
-                    {
-                        fh = fh.Substring(0, numIndex) + "-" + fh.Substring(numIndex, fh.Length - numIndex);
-                        if (IdSet.Contains(fh)) continue;
-                        av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
-                    *//*else if (fh.IndexOf("_") != -1)
-                    {
-                        fh = fh.Replace("_", "-");
-                        if (IdSet.Contains(fh)) continue;
-                        av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
-                    else if (fh.IndexOf("-") != -1)
-                    {
-                        fh = fh.Replace("-", "_");
-                        if (IdSet.Contains(fh)) continue;
-                        av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }*//*
-                }//foreach match
-                Match mat = new Regex(@"[Nn]\d{3,5}").Match(name);
-                if (mat.Success)
-                {
-                    string fh = mat.Groups[0].Value;
-                    if (!IdSet.Contains(fh))
-                    {
-                        Av av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
+                    textBox2.AppendText("->" + name + "\r\n");
                 }
-                mat = new Regex("[A-Za-z]+-[A-Za-z0-9]+").Match(name);
-                if (mat.Success)
+                catch 
                 {
-                    string fh = mat.Groups[0].Value;
-                    if (!IdSet.Contains(fh))
-                    {
-                        Av av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
+                    textBox3.AppendText(name + "\r\n");
                 }
-                mat = new Regex("[A-Za-z]+[0-9]{3,8}").Match(name);
-                if (mat.Success)
-                {
-                    string fh = mat.Groups[0].Value;
-                    int numIndex = getNumberIndex(fh);
-                    fh = fh.Substring(0, numIndex) + "-" + fh.Substring(numIndex, fh.Length - numIndex);
-                    if (!IdSet.Contains(fh))
-                    {
-                        Av av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
-                }
-                mat = new Regex("[0-9]+-[0-9]+[-]?[0-9]+").Match(name);
-                if (mat.Success)
-                {
-                    string fh = mat.Groups[0].Value;
-                    if (!IdSet.Contains(fh))
-                    {
-                        Av av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
-                }
-                mat = new Regex("[A-Za-z]+[-_]?[0-9]{3,8}").Match(name);
-                if (mat.Success)
-                {
-                    string fh = mat.Groups[0].Value;
-                    fh = fh.Replace("_", "-");
-                    if (!IdSet.Contains(fh))
-                    {
-                        Av av = new Av();
-                        av.Id = fh;
-                        av.Path = folder;
-                        avs.Add(av);
-                        textBox2.AppendText("->" + fh + "\r\n");
-                        IdSet.Add(fh);
-                    }
-                }*/
 
             }//for
 
