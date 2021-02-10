96c791bae99675dc2746ffaffb9fd09b1baa2d93
ywl <253612442@qq.com>
2021-02-10 17:39:39 +0800

    Sync

--- AvCoverDownloader/AvCoverDownloader/Av.cs
+++ AvCoverDownloader/AvCoverDownloader/Av.cs
@@ -12,5 +12,13 @@ namespace AvCoverDownloader
         public string Path { get; set; }
         public string Name { get; set; }
         public string Html { get; set; }
+
+        public bool CD1 { get; set; }
+
+        public List<String> stars = new List<string>();
+        public List<String> tags = new List<string>();
+
+        public string date { get; set; }
+        public string title { get; set; }
     }
 }
--- AvCoverDownloader/AvCoverDownloader/Collector.cs
+++ AvCoverDownloader/AvCoverDownloader/Collector.cs
@@ -154,9 +154,13 @@ namespace AvCoverDownloader
                 {
                     //match = new Regex(@"<h3>(?<title>.*?)</h3>[\s\S]*?<p><span class=""header"">發行日期:</span>(?<date>.*?)</p>").Match(av.Html);
                     match = new Regex(@"<h3>(?<title>[\s\S]*?)</h3>[\s\S]*?<p><span class=""header"">發行日期:</span>(?<date>.*?)</p>").Match(av.Html);
+                    
                     string newPath = null;
                     if (match.Success)
                     {
+                        av.date = match.Groups["date"].Value.Trim();
+                        av.title = match.Groups["title"].Value.Trim();
+
                         string newfolderName = match.Groups["date"].Value.Trim() + " " + match.Groups["title"].Value.Trim();
                         newfolderName = newfolderName.Replace("\\", "‖");
                         newfolderName = newfolderName.Replace("/", "‖");
@@ -174,10 +178,24 @@ namespace AvCoverDownloader
                             rBuilder.Replace(rInvalidChar.ToString(), string.Empty);
 
                         int fg = av.Path.LastIndexOf("\\");
-                        string basePath = av.Path.Substring(0, fg).Trim();
+                        string baseFilePath = av.Path.Substring(0, fg).Trim();
                         System.IO.DirectoryInfo folder = new System.IO.DirectoryInfo(av.Path);
-                        folder.MoveTo(basePath+ "\\" + newfolderName);
-                        newPath = basePath + "\\" + newfolderName;
+
+                        string apath = "";
+                        qb = "<li class=\"active\"><a href=\"" + basePath + "/\">";
+                        if (av.Html.IndexOf(qb) != -1)
+                        {
+                            apath += "骑兵\\";
+                        }
+                        else
+                        {
+                            apath += "步兵\\";
+                        }
+                        MoveFolder(av.Path, baseFilePath + "\\" + apath + newfolderName);
+                        Directory.Delete(av.Path, true);
+                        //folder.MoveTo();
+                        newPath = baseFilePath + "\\" + apath + newfolderName;
+                        av.Path = newPath;
                     } 
                     else
                     {
@@ -203,6 +221,7 @@ namespace AvCoverDownloader
                                 if (extNames[i].ToLower().Equals(f.Extension.ToLower()))
                                 {
                                     string newName = newPath + "\\" + av.Id + (x > 1 ? "-cd" + y : "") + f.Extension;
+                                    av.CD1 = x > 1;
                                     try
                                     {
                                         //记录文件名称
@@ -224,8 +243,16 @@ namespace AvCoverDownloader
                     _syncContext.Post(OutError, av.Id + " :修改AV名称失败" + e.Message);
                 }
 
-
                 _syncContext.Post(OutLog, av.Id);
+
+                try
+                {
+                    creNfo(av);
+                }
+                catch (Exception e)
+                {
+                    _syncContext.Post(OutLog, av.Id + ":nfo 创建失败" + e.Message);
+                }
             }
         }
 
@@ -250,6 +277,122 @@ namespace AvCoverDownloader
                 sw1.WriteLine(content);
                 sw1.Close();
             }
+        }
+
+        /// <summary>
+        /// 移动文件夹中的所有文件夹与文件到另一个文件夹  
+        /// </summary>
+        /// <param name="sourcePath">源文件夹</param>
+        /// <param name="destPath">目标文件夹</param>
+        public static void MoveFolder(string sourcePath, string destPath)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败：" + ex.Message);
+                    }
+                }
+                //获得源文件下所有文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //覆盖模式
+                    if (File.Exists(destFile))
+                    {
+                        File.Delete(destFile);
+                    }
+                    File.Move(c, destFile);
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //Directory.Move必须要在同一个根目录下移动才有效，不能在不同卷中移动。
+                    //Directory.Move(c, destDir);
+
+                    //采用递归的方法实现
+                    MoveFolder(c, destDir);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在！");
+            }
+        }
+
+
+        public static void creNfo(Av av)
+        {
+            Match mat = null;
+            string p = av.title.Replace(av.Id, "").Trim();
+            //string plot = "google:"+Translate.google(p) + " youdao:"+Translate.youdao(p);
+
+            string fileStr = av.Path + "\\" + av.Id + (av.CD1 ? "-cd1" : "") + ".nfo";
+            FileStream fs1 = new FileStream(fileStr, FileMode.Create, FileAccess.Write);//创建写入文件
+            StreamWriter sw = new StreamWriter(fs1);
+            sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>");
+            sw.WriteLine("<movie>");
+            //sw.WriteLine("<plot>" + plot + "</plot>");
+            sw.WriteLine("<outline/>");
+            sw.WriteLine("<lockdata>false</lockdata>");
+            sw.WriteLine("<title>" + av.Id + "</title>");
+            sw.WriteLine("<originaltitle>" + p + "</originaltitle>");
+            mat = new Regex(@"<p><span class=""header"">導演:</span> <a href="".*?"">(.*?)</a></p>").Match(av.Html);
+            if (mat.Success)
+            {
+                sw.WriteLine("<director>" + mat.Groups[1].Value.Trim() + "</director>");
+            }
+            sw.WriteLine("<year>" + av.date.Substring(0, 4) + "</year>");
+            sw.WriteLine("<premiered>" + av.date + "</premiered>");
+            sw.WriteLine("<releasedate>" + av.date + "</releasedate>");
+
+            mat = new Regex(@"<p><span class=""header"">系列:</span> <a href="".*?"">(.*?)</a></p>").Match(av.Html);
+            if (mat.Success)
+            {
+                sw.WriteLine("<genre>" + mat.Groups[1].Value.Trim() + "</genre>");
+            }
+
+            mat = new Regex(@"<p><span class=""header"">長度:</span>\s*(\d*)分鐘</p>").Match(av.Html);
+            if (mat.Success)
+            {
+                sw.WriteLine("<runtime>" + mat.Groups[1].Value.Trim() + "</runtime>");
+            }
+
+            mat = new Regex(@"<p><span class=""header"">製作商:</span> <a href="".*?"">(.*?)</a>").Match(av.Html);
+            if (mat.Success)
+            {
+                sw.WriteLine("<studio>" + mat.Groups[1].Value.Trim() + "</studio>");
+            }
+
+            MatchCollection tags = new Regex(@"<span class=""genre""><a href="".*?"">(.*?)</a></span>").Matches(av.Html);
+            foreach (Match m in tags)
+            {
+                sw.WriteLine("<tag>" + m.Groups[1].Value.Trim() + "</tag>");
+            }
+            MatchCollection actors = new Regex(@"<div class=""photo-frame"">\s*<img src=""(.*?)"" title=""(.*?)"">\s*</div>\s*<span>(.*?)</span>").Matches(av.Html);
+            foreach (Match m in actors)
+            {
+                sw.WriteLine("<actor>");
+                sw.WriteLine("<name>" + m.Groups[2].Value.Trim() + "</name>");
+                //sw.WriteLine("<role></role>");
+                sw.WriteLine("<type>Actor</type>");
+                sw.WriteLine("<thumb>" + m.Groups[1].Value.Trim() + "</thumb>");
+                sw.WriteLine("</actor>");
+            }
+            sw.WriteLine("</movie>");
+            sw.Close();
+            fs1.Close();
         }
 
         private void OutLog(object state)
